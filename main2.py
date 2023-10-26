298cc43ca7b2fab86cf2455c64a358a464c6b9fe
clever88 <prostopavel2020@yandex.ru>
2023-10-26 18:00:52 +0800

    first commit

---
+++ main2.py
@@ -0,0 +1,20 @@
+length = float(input("Длина: "))
+width = float(input("Ширина: "))
+height = float(input("Высота: "))
+
+# Вычисляем площадь и объем комнаты
+area = length * width
+volume = length * width * height
+
+# Выводим результат
+print("Площадь комнаты:", area)
+print("Объем комнаты:", volume)
+
+ 
+
+
+#Длина: 8
+#Ширина: 5  
+#Высота: 2.8
+#Площадь комнаты: 40.0
+#Объем комнаты: 112.0
